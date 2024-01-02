c43e91d711ec26c88dc990aeba216d70fa41fc23
Joshua Ransom <personal.jlr@gmail.com>
2024-01-01 19:58:22 -0600

    feat: small program to take in a file for triangular numbers
    used in a test for `dataannotation.tech`

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
+import java.lang.StringBuilder
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val file = File("message_file.txt")
+    println(decode(file))
+}
+
+fun decode(message_file: File): String {
+    val inputMap = mutableMapOf<Int, String>()
+    message_file.forEachLine {
+        val line = it.split(" ")
+        val number = line[0].toInt()
+        inputMap[number] = line[1]
+    }
+
+    val sb = StringBuilder()
+    val sortedMap = inputMap.toSortedMap()
+
+    for (i in 1..sortedMap.size) {
+        val value = i * (i + 1) / 2
+        if (value > sortedMap.lastKey()) {
+            break
+        }
+        sb.append(sortedMap[value]).append(" ")
+    }
+
+    if (sb.isNotEmpty()) {
+        sb.setLength(sb.length - 1)
+    }
+
+    return sb.toString()
 }
